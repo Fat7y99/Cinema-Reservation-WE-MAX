f853aeaf18f8175bfa7cb2c0fef10fe7b29f50b4
Fat7y99 <67173807+Fat7y99@users.noreply.github.com>
2022-01-01 19:02:03 +0200

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,8 +19,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "857786f541d84435f5715515c00fb92c",
-"/": "857786f541d84435f5715515c00fb92c",
+"index.html": "b6cbe27a8be27d4926ac732aa5a88e51",
+"/": "b6cbe27a8be27d4926ac732aa5a88e51",
 "main.dart.js": "044866745a0b7c5cd4a6872ea530f9bf",
 "manifest.json": "2d7882af26e2438bd7c0117f5539c65a",
 "version.json": "e6fca44d692490d6f8a52dd71a014efd"
